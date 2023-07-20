1c67caf4b339ee867b0fe563d641b5375eabb42f
luisgustavorr <luisgustavo20061@gmail.com>
2023-07-20 11:31:37 -0300

    Atualizando vendor

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '30021dad507c1cbf3db1f048a831f8a2b8eff36e',
+        'reference' => '5ccbe39b062ffc0a487c92c7a9cc8164132dc0ae',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '30021dad507c1cbf3db1f048a831f8a2b8eff36e',
+            'reference' => '5ccbe39b062ffc0a487c92c7a9cc8164132dc0ae',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
